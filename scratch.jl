f2f4374e663d02d96972cdb3193889fc7fc26cf0
a-poor <45295232+a-poor@users.noreply.github.com>
2021-04-09 08:48:15 -0700

    Adding @trycatch to scratch.jl

--- scratch.jl
+++ scratch.jl
@@ -29,6 +29,30 @@ const CONTENT_TYPES = Dict{Symbol,String}(
 )
 
 
+"""
+    @trycatch expr
+    
+Convenience macro which wraps an expression in a `try/catch` block.
+If the expression throws an error, it will be printed with the 
+`@error` macro.
+# Examples
+julia> @trycatch 1 + 1
+2
+julia> @trycatch 1 + "dog"
+┌ Error: MethodError(+, (1, "dog"), 0x0000000000006cbc)
+└ @ Main REPL[13]:6
+"""
+macro trycatch(expr)
+    quote
+        try
+            $expr
+        catch err
+            @error err
+        end
+    end
+end
+
+
 # Functions for Response Types
 
 function text(data::Any ; status::Int = 200, header::Array{Pair{String,String},1} = Pair{String,String}[])
