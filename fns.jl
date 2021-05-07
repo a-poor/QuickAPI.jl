17a3f89e9b54a5a4751bf5434aa9055578544767
Austin Poor <45295232+a-poor@users.noreply.github.com>
2021-05-07 16:08:53 -0700

    Adding notes to fns.jl for adding future code

--- fns.jl
+++ fns.jl
@@ -1,6 +1,20 @@
 
 module fns
 
+
+abstract type FunctionArgument end
+
+struct Arg <: FunctionArgument end
+struct KwArg <: FunctionArgument end
+struct OptionalArg <: FunctionArgument end
+
+
+# Code to parse argument Expr into 
+# `FunctionArgument`s
+# ...
+
+
+
 #= 
 Function for Extracting Parameters
 Accepts lambda functions only. In the form:
@@ -54,3 +68,43 @@ end
 
 
 end # module
+
+
+
+
+
+# a = Argument
+# o = Optional Argument
+# k1 = Keyword Argument
+# k2 = Keyword Argument
+fn = :((a::Int, o::Float64 = 1. ; k1::Int = 1, k2::Float64 = 2.) -> 42)
+
+all_args = fn.args[1].args
+#=
+3-element Vector{Any}:
+ :($(Expr(:parameters, :($(Expr(:kw, :(k1::Int), 1))), :($(Expr(:kw, :(k2::Float64), 2.0))))))
+ :(a::Int)
+ :(o::Float64 = 1.0)
+=#
+
+arg = all_args[2] # :(a::Int)
+
+opt_arg = all_args
+
+
+
+kwargs = all_args[1]
+
+
+
+#=
+
+Recursively deconstruct function argument `Expr`.
+
+* Args are the simplest :(argname::ArgType)
+* Optional Args have head `:(=)` and args `[:(name::Type),"Default Value"]`
+* All Kwargs are grouped together into a single expression with head `:parameters`
+* Individual kwargs have head `:kw` and args `[:(kwarg_name::Type),"Default Value"]`
+=#
+
+
